b62a4dadc708c6fbd78495ef9497a2e9475869c6
rickdev8 <rique88858@gmail.com>
2026-06-27 05:53:21 -0300

    latest cmtt

--- app/services/list-players-service.ts
+++ app/services/list-players-service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export async function ListPlayers(){
-    return await axios.get("https://quiz-arena-backend-3sze.onrender.com")
+    return await axios.get("https://quiz-arena-backend-3sze.onrender.com/listen-users")
 }
